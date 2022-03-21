2f52e8bfa1abe97571b39dc1beff57e8d9c59e5a
Petri <peewter@gmail.com>
2022-03-21 17:43:12 +0200

    add pause menu and related artwork

---
+++ Assets/Code/PauseMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void QuitGame()
+    {
+        print("Quit Game!");
+        Application.Quit();
+    }
+}
